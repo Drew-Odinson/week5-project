6b81a7b6d5da189905e6c4eea0944aec522ef334
Drew <drew.ledford2022@gmail.com>
2023-09-11 21:51:15 -0500

    Done and working

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # week5-project
-Work Day Scheduler Starter Code
+Work Day Scheduler 
+
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- My motivation is to build calendar so that I can add to it as my skill impove.
+- I took on the calendar project on to improve my skills and apply everything I have learned to this point of the bootcamp program.
+- Working on this project really pushed me to see what I have learned and can repeat.
+- In this project I found researching code all over the place. I want to keep building my own skills so I do not rely on searches to find code instead build it myself. 
+- I am learning to pay close attention to details. This is one of the biggest things for me. I found myself going faster and faster with little mistakes here and there adding up. 
+
+
+## Installation
+
+To start this project I had to get the Git file provided to me, copy it to my local drive and upload it to both my VScode as well as a new GitHub to perform all actions needed.
+
+## Usage
+The webpage has an active timer as well as a date tracker on it that updates to you when you open it and have it running. 
+If you add in your own event into the calendar and click the save button on the right, it will save your event into local storage. 
+
+![Top view](<Screenshot 2023-09-11 214342.png>)
+![Bottom of page](<Screenshot 2023-09-11 214352.png>)
+![Stored data](<Screenshot 2023-09-11 214951.png>)
+
+## Credits
+
+
+
+## License
+
+See MTC license on GitHub
+
+
+
--- script.js
+++ script.js
@@ -1,23 +1,87 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  // Display current date, tried other formats, really like the "dddd, MMMM D, YYYY" the best
+  const currentDate = dayjs().format("dddd, MMMM D, YYYY");
+  $("#currentDay").text(currentDate); //Took forever to figure out I was missing the $ symbol
+
+  // The for loop to go through each hour
+  $(".time-block").each(function () {
+    const hourString = $(this).attr("id").split("-")[1];
+    const hour = parseInt(hourString);
+    const currentHour = parseInt(dayjs().format("H"));
+
+    //The if statement to cycle through past,present, and future hours
+    if (hour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("present future");
+    } else if (hour === currentHour) {
+      $(this).addClass("present");
+      $(this).removeClass("past future");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("past present");
+    }
+
+    // This will "get" or pull the saved items
+    const savedData = localStorage.getItem(`hour-${hour}`);
+    if (savedData) {
+      $(this).find("textarea").val(savedData);
+    }
   });
+
+  // The event listener for buttons
+  //Checked, the ".saveBtn" does store to local with time
+  $(".saveBtn").on("click", function () {
+    const timeBlock = $(this).closest(".time-block");
+    const hour = timeBlock.attr("id").split("-")[1];
+    const textAreaValue = timeBlock.find("textarea").val();
+
+    // Save users input to localStorage
+    localStorage.setItem(`hour-${hour}`, textAreaValue);
+  });
+});
+
+
+// Adding a current clock for easy use to the user
+const currentDate = dayjs().format("dddd, MMMM D, YYYY");
+$("#currentDay").text(currentDate);
+
+//This was a neat find, going to use it alot in the future
+function updateClock() {
+  const currentTime = dayjs().format("h:mm:ss A");
+  $("#liveClock").text(currentTime);
+}
+
+updateClock();//Call for the clock to keep updating itself
+setInterval(updateClock, 1000);//Forgot it tracks time in milseconds
+
+// This should update the colors in the boxes, and after a few hundrend tries it does :)
+function updateTimeBlocks() {
+  const currentHour = dayjs().hour(); // get current hour
+
+  $(".time-block").each(function () { //$$$$$-need to ask about these
+    const blockHour = parseInt($(this).attr("id").split("-")[1]); // get the hour from the id
+
+    if (blockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+
+  });
+}
+
+// Call for the function to set the initial class 
+updateTimeBlocks();
+
+// )
+// 60,000 milseconds would equal one hour which is when we want the colors to change
+//I kept trying to use 6000
+setInterval(updateTimeBlocks, 60000);
+
+
+  //Need to ask if I need to put in a reset function into this
